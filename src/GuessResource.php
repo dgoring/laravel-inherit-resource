3c80486c662ff80c1c9989e295b5c63c90f4636a
David Goring <david.lj.goring@gmail.com>
2020-12-19 04:37:46 +0000

    Fix for conflicting input types

--- src/GuessResource.php
+++ src/GuessResource.php
@@ -92,9 +92,7 @@ trait GuessResource
       return $this->resource;
     }
 
-    $class = $this->getClassName();
-
-    if($id = request()->{$this->getInstanceName()})
+    if($id = request()->route($this->getInstanceName()))
     {
       return $this->resource = $this->collection()->findOrFail($id);
     }
